cbdbc225b51fc70f89d90074c1b3768c0097c41d
Emiliano.Santi <emiliano.santi@wnco.com>
2014-09-19 13:00:23 -0500

    Emiliano Santi - YT-onKeyPressFirefox - FirefoxEnter BUG Fixied

--- src/client/common/components/yt-input.js
+++ src/client/common/components/yt-input.js
@@ -6,7 +6,7 @@ var YTInput = React.createClass({
 
 	render: function() {
 		return (
-			<input className="yt--input-url" id="inputurl" placeholder="Insert url here to add" onChange={this.props.onChange} onKeyPress={this.props.onKeyPress} value={this.props.value}>
+			<input className="yt--input-url" id="inputurl" placeholder="Insert url here to add" onChange={this.props.onChange} onKeyUp={this.props.onKeyUp} value={this.props.value}>
 				{this.props.children}
 			</input>
 		)
