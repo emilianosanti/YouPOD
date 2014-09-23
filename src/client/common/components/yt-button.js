3058793da33672a126ae1b8749461f4741d6a38e
Emiliano.Santi <emiliano.santi@wnco.com>
2014-09-23 11:15:29 -0500

    Emiliano Santi - YT-VideoBanner-v1 - Delete video Playing v1

--- src/client/common/components/yt-button.js
+++ src/client/common/components/yt-button.js
@@ -6,7 +6,7 @@ var YTButton = React.createClass({
 	render: function() {
 		var self = this;
 		return (
-				<button onClick={this.props.onClick} className = "yt--button" id="buttonid" title="Click To Add" >Add
+				<button onClick={this.props.onClick} className = "yt--button" id="buttonid" title="Click To Add" >
 				 {this.props.children}
 				 </button>
 			)
